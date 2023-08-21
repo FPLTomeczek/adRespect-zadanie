a3b5c6a70ab4350c12b670c4a578ebd6cd858270
FPLTomeczek <tomaszs18m@gmail.com>
2023-08-21 18:43:19 +0200

    fix: webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require("path");
-const glob = require("glob");
 
 module.exports = {
   mode: "development",
-  entry: toObject(glob.sync(".src/logic/*.js*")),
+  entry: [
+    "./src/logic/navbar.js",
+    "./src/logic/projects.js",
+    "./src/logic/hero.js",
+  ],
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index.js",
@@ -11,23 +14,15 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: "file-loader",
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
           },
-        ],
+        },
       },
     ],
   },
 };
-
-function toObject(paths) {
-  var ret = {};
-
-  paths.forEach(function (path) {
-    ret[path.split("/").slice(-1)[0]] = path;
-  });
-
-  return ret;
-}
